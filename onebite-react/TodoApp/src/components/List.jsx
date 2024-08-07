b07290bd898bd1d1107588f962cb915c4b9864fa
LEE-DA-EUN <delee3064@gmail.com>
2024-08-07 23:49:59 +0900

    리스트 검색기능 추가

--- onebite-react/TodoApp/src/components/List.jsx
+++ onebite-react/TodoApp/src/components/List.jsx
@@ -1,15 +1,39 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => { //검색기능
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => { //필터링된 리스트 리턴
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
