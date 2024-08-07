3d425a6ebacca104ec3f859e4ae40a47bde174ad
LEE-DA-EUN <delee3064@gmail.com>
2024-08-07 17:21:24 +0900

    useEffect 사용

--- onebite-react/CounterApp/src/App.jsx
+++ onebite-react/CounterApp/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Viewer from "./components/Viewer";
 import Controller from './components/Controller';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(()=>{
+    console.log(`count: ${count} / input: ${input}`)
+  }, [count, input]) //의존성배열 = deps
 
   const onClickButton = (value) => {
     setCount(count+value);
@@ -14,10 +19,15 @@ function App() {
     <div className="App">
       <h1>Simple Counter</h1>
         <section>
-        <Viewer count={count} />
+          <input value={input} onChange={(e)=>{
+            setInput(e.target.value)
+          }}/>
         </section>
         <section>
-        <Controller onClickButton={onClickButton}/>
+          <Viewer count={count} />
+        </section>
+        <section>
+          <Controller onClickButton={onClickButton}/>
         </section>
     </div>
   );
