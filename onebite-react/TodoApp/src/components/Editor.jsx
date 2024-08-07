10fe8c12f07cd07b823fc5b6c9fc4fc38cfb3e69
LEE-DA-EUN <delee3064@gmail.com>
2024-08-07 23:37:36 +0900

    TodoItem 추가하는 여러 기능 구현

--- onebite-react/TodoApp/src/components/Editor.jsx
+++ onebite-react/TodoApp/src/components/Editor.jsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Editor.css";
 
 const Editor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const inputRef = useRef();
 
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
 
+  const onKeydown = (e) => { //엔터키로 아이템 추가
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   const onSubmit = () => {
+    if (content === "") { //빈문자열 입력방지
+      inputRef.current.focus();
+      return;
+    }
     onCreate(content);
+    setContent(""); //입력후 인풋초기화
   };
 
   return (
     <div className="Editor">
       <input
+        ref={inputRef}
         value={content}
         onChange={onChangeContent}
+        onKeyDown={onKeydown}
         placeholder="새로운 Todo..."
       />
       <button onClick={onSubmit}>추가</button>
