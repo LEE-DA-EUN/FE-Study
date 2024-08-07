cdc1d6eac0b2f1003038cacf81f190690e087240
LEE-DA-EUN <delee3064@gmail.com>
2024-08-07 15:55:24 +0900

    UI 구현

--- onebite-react/CounterApp/src/App.jsx
+++ onebite-react/CounterApp/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+import Viewer from "./components/Viewer";
+import Controller from './components/Controller';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <h1>Simple Counter</h1>
+        <section>
+        <Viewer />
+        </section>
+        <section>
+        <Controller />
+        </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
