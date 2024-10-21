3a79771fc0ccfda731dbcfc2c7a748438a60c520
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-10-21 11:18:05 +0900

    4-9 서로소 유니온 타입

---
+++ TypeScript/onebite-typescript/section3/src/chapter9.ts
@@ -0,0 +1,77 @@
+type Admin = {
+  tag: "ADMIN";
+  name: string;
+  kickCount: number;
+};
+
+type Member = {
+  tag: "MEMBER";
+  name: string;
+  point: number;
+};
+
+type Guest = {
+  tag: "GUEST";
+  name: string;
+  visitCount: number;
+};
+
+// 서로소 유니온 타입
+type User = Admin | Member | Guest;
+
+function login(user: User) {
+  switch (user.tag) {
+    case "ADMIN": {
+      console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
+      break;
+    }
+    case "MEMBER": {
+      console.log(`${user.name}님 현재까지 ${user.point}모았습니다.`);
+      break;
+    }
+    case "GUEST": {
+      console.log(
+        `${user.name}님 현재까지 ${user.visitCount}번 방문하셨습니다.`
+      );
+      break;
+    }
+  }
+}
+
+// 두번째 예시
+type LoadingTask = {
+  state: "LOADING";
+};
+
+type FailedTask = {
+  state: "FAILED";
+  error: {
+    message: string;
+  };
+};
+
+type SuccessTask = {
+  state: "SUCCESS";
+  response: {
+    data: string;
+  };
+};
+
+type AsyncTask = LoadingTask | FailedTask | SuccessTask;
+
+function processResult(task: AsyncTask) {
+  switch (task.state) {
+    case "LOADING": {
+      console.log("로딩 중");
+      break;
+    }
+    case "FAILED": {
+      console.log(`에러 발생 : ${task.error.message}`);
+      break;
+    }
+    case "SUCCESS": {
+      console.log(`성공 : ${task.response.data}`);
+      break;
+    }
+  }
+}
